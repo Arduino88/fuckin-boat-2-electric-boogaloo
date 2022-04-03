02cce570b6a1624963abc90d682c0c3bc40a5129
Gabriel Tadeka <simonharrisonsiena@gmail.com>
2022-04-03 01:50:17 -0400

    initialize github repository

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js')
-
-const TOKEN = "OTU5OTcwODgwMzU4ODYyOTAw.YkjojQ.yFXV-EXe4vo3SbqICMeOHPK_ePo"
+require('dotenv').config()
 
 const client = new Discord.Client({
     intents: [
@@ -10,7 +9,7 @@ const client = new Discord.Client({
     ]
 })
 
-client.login(TOKEN)
+client.login(process.env.TOKEN)
 
 const channel = client.channels.cache.get(channel => channel.name === '928817714645053502')
 
@@ -25,15 +24,5 @@ client.on("messageCreate",  (message) => {
         //channel.send('hello!')
   //.then(message => console.log(`Sent message: ${message.content}`))
   //.catch(console.error);
-    }
-})
-
-client.on("messageCreate", async msg => {
-    if(msg.guild && !msg.author.bot && msg.content.includes("!embed")){
-        const embed = new Discord.MessageEmbed().setTitle("CUMOINMTRASSBROTHER").setColor("BLUE").setThumbnail(client.user.displayAvatarURL()).setTimestamp()
-        
-        client.channels.cache.get('channelID').send('hi');
-        
-        // msg.channel.send({embeds:[embed]})
     }
 })
